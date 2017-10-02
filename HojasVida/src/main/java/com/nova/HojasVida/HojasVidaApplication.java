4373853c2aa8e4e5e97d05de8931da2fc834dacd
Hburitica <hburitica@intergrupo.com>
2017-10-02 13:42:43 -0500

    Se crea proyecto gradle

---
+++ HojasVida/src/main/java/com/nova/HojasVida/HojasVidaApplication.java
@@ -0,0 +1,12 @@
+package com.nova.HojasVida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HojasVidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HojasVidaApplication.class, args);
+	}
+}
